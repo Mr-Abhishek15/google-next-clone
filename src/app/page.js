2b9140e8812a7c7755e1cc80612555ab42a4b824
abhishek <shek79000@gmail.com>
2024-04-07 09:02:04 +0530

    updated the clone

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
+import HomeHeader from "@/component/HomeHeader";
+
 export default function Home(){
   return(
-    <h1 className='text-red-500'>Home</h1>
+    <> 
+    <HomeHeader />
+    </>
   )
 }
